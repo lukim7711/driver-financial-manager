02af76fcd34040b30fa2d2685fb43df045355825
budbud <124249458+lukim7711@users.noreply.github.com>
2026-02-13 22:31:03 +0700

    fix(frontend): update to React Router 7 modern API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import QuickInput from './pages/QuickInput'
 import Debts from './pages/Debts'
